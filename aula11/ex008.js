41890a0f38ed71efe3f6e59cf7ce4091bd184efd
joaoalvesd <110297563+joaoalvesd@users.noreply.github.com>
2022-08-21 14:05:40 -0300

    novo curso

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+if(vel > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade máxima. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula12/ex013.js
@@ -0,0 +1,21 @@
+var agora = new Date()
+var diasem = agora.getDay()
+diasem =3
+switch(diasem){
+    case 0:
+        console.log('Domingo')
+        break
+        case 1: console.log('Segunda')
+        break
+        case 2: conseole.log('terça')
+        break
+        case 3: console.log('quarta')
+        break
+        case 4: console.log('quinta')
+        break
+        case 5: console.log('sexta')
+        case 6: console.log('sabado')
+
+        default:console.log(' [ERRO] dia invalido')
+
+}
